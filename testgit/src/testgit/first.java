316a928a3a39679fe0d867ca0a0b7db4ebaf995c
yoYWH <hedgeh0g@yeah.net>
2020-06-05 09:38:59 +0800

    把本地工程添加到git

---
+++ testgit/src/testgit/first.java
@@ -0,0 +1,11 @@
+package testgit;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello" );
+        System.out.println("hello" );
+        System.out.println("hello" );
+        System.out.println("hello" );
+        System.out.println("hello" );
+    }
+}
